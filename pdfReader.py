1c716d8c248c352d6c5a1da1b9023cadee45af79
Rolo <rolo_lamperouge@foxmail.com>
2017-07-10 04:57:41 +0800

    start to devide pages

--- pdfReader.py
+++ pdfReader.py
@@ -1,21 +1,21 @@
 
-import textract
-import codecs
-
-def readPdfToTextInUTF8(pdfPathAsInput, txtNameAsOutput = "output.txt"):
-    text = textract.process(pdfPathAsInput)
-    strResult = text.strip().decode('utf-8')
-    file = codecs.open(txtNameAsOutput,'w','utf-8')
-    file.write(strResult)
-    file.close()
-
-def readPdfToHtml(pdfPathAsInput):
-    pass
-
-file = codecs.open("output.txt","r","utf-8")
-for line in file.readlines():
-    print(line)
-file.close()
+# import textract
+# import codecs
+#
+# def readPdfToTextInUTF8(pdfPathAsInput, txtNameAsOutput = "output.txt"):
+#     text = textract.process(pdfPathAsInput)
+#     strResult = text.strip().decode('utf-8')
+#     file = codecs.open(txtNameAsOutput,'w','utf-8')
+#     file.write(strResult)
+#     file.close()
+#
+# def readPdfToHtml(pdfPathAsInput):
+#     pass
+#
+# file = codecs.open("output.txt","r","utf-8")
+# for line in file.readlines():
+#     print(line)
+# file.close()
 
 #printerr(text)
 
@@ -44,3 +44,36 @@ def convert_pdf(path, page=1):
 file  = r'ANA domestic.pdf'
 print(convert_pdf(file))
 """
+
+from bs4 import BeautifulSoup
+from bs4 import UnicodeDammit
+from urllib.request import urlopen
+import re
+import sys
+
+def deleteFiles():
+    html = open("./ANA domestic.html",encoding='utf-8')
+    data = re.sub("(<img).*(\"\/>)","",html.read())
+    html.close()
+    html = open("./ANA domesticNoPhoto.html",'w',encoding='utf-8')
+    html.write(data)
+    html.flush()
+    html.close()
+# html = html.decoding('ascii').encoding('utf-8')
+# try:
+#     bs = BeautifulSoup(html,"lxml")
+# except UnicodeDecodeError as e:
+#     print(e.object)
+#
+def handlePage(page):
+    pass
+
+html = open("./ANA domestic.html",encoding='utf-8')
+bs = BeautifulSoup(html,"lxml")
+pages = bs.findAll("div",{"class":re.compile("pf(.)*")})
+for page in pages:
+    handlePage(page)
+
+
+# for b in a:
+#    print(b)
