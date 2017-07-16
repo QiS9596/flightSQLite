6e22ca24328f7c2e336f5cb1b583c2881e8a1cc8
Rolo <rolo_lamperouge@foxmail.com>
2017-07-17 06:57:00 +0800

    gather info, wrongly collect other data, should try to use regex to select them

--- pdfReader.py
+++ pdfReader.py
@@ -47,11 +47,16 @@ class shortedTag:
             list[min] = temp
         return list
 
+#class augmentedList(list):
 
 
 
 result = {}
 handledResult = {}
+page_no = "data-page-no"
+titles = "titles"
+times = "times"
+fNo = "flightNo"
 def deleteFiles():
     html = open("./ANA domestic.html",encoding='utf-8')
     data = re.sub("(<img).*(\"\/>)","",html.read())
@@ -62,23 +67,46 @@ def deleteFiles():
     html.close()
 
 def handlePage(page):
-    Titles = page.findAll("div",{"class":re.compile("(t m0 .* ffb fs25.*)")})
+    Titles = page.findAll("div",{"class":re.compile("(t m0 .* ffc fs1d.*)")})
     Times = page.findAll("div",{"class":re.compile("(t me .* ffd fs24.*)")})
-    if Titles.__len__()>=1:
-        result[page["data-page-no"]] = {}
-        result[page["data-page-no"]]["titles"] = []
-        result[page["data-page-no"]]["times"] = []
-    for title in Titles:
-        #print(title)
-        info = title["class"]
+    FlightNo = page.findAll("div",{"class":re.compile("(t m12 .* ff11 fs17.*)")})
 
-        result[page["data-page-no"]]["titles"].append(shortedTag(info[2],info[3],info[4],title.text))
+    if Titles.__len__()>=1:
+        result[page[page_no]] = {}
+        result[page[page_no]][titles] = []
+        result[page[page_no]][times] = []
+        result[page[page_no]][fNo] = []
+    for title in Titles:
+        info = title["class"]
+        result[page[page_no]][titles].append(shortedTag(info[2], info[3], info[4], title.text))
 
 
     for time in Times:
         info = time["class"]
-        result[page["data-page-no"]]["times"].append(shortedTag(info[2],info[3],info[4],title.text))
+        result[page[page_no]][times].append(shortedTag(info[2], info[3], info[4], title.text))
 
+    for flights in FlightNo:
+        info = flights["class"]
+        result[page[page_no]][fNo].append(shortedTag(info[2], info[3], info[4], title.text))
+
+def handleOneType(resultList,rawDataList):
+    for a in rawDataList:
+        flag = True
+        for b in resultList:
+            if a.compareTo(b[0],"x")==0:
+                b.append(a)
+                flag = False
+        if flag:
+            resultList.append([a])
+    return resultList
+
+def handleResult():
+    for p in result.keys():
+        handledResult[p] = {}
+        for key in result[p].keys():
+            handledResult[p][key] = []
+        for key in handledResult[p].keys():
+            handledResult[p][key] = handleOneType(handledResult[p][key],result[p][key])
 
 
 html = open("./ANA domestic.html",encoding='utf-8')
@@ -86,9 +114,16 @@ bs = BeautifulSoup(html,"lxml")
 pages = bs.findAll("div",{"class":re.compile("pf(.)*")})
 for page in pages:
     handlePage(page)
-for a in result.keys():
-    handledResult[a] = {}
-    #handledResult[a]["Titles"] = []
+handleResult()
+a = sorted(handledResult.keys())[0]
+print(a)
+for key in handledResult[a].keys():
+    print(key)
+    print(handledResult[a][key].__len__())
+    for array in handledResult[a][key]:
+        print(array[0].x1)
+        for ele in array:
+            print(ele.text)
 
 
 
