906b69e2a12516768509c075acdba7014733de6b
Rolo <rolo_lamperouge@foxmail.com>
2017-10-04 11:05:18 +0800

    fix termination check bug of Best first search

--- BFS.py
+++ BFS.py
@@ -1,10 +1,11 @@
 from asyncio import PriorityQueue
 import datetime
 import sql
+import util
 startDatetime = None  #the start date time set by user
 endDatetime = None   #the arrival deadline set by user
 TIME_END_FLAG = 10000 #some big constant used in eval()
-DESTINATION_FLAG = 10000  #some big constant
+DESTINATION_FLAG = 100000  #some big constant
 MAXIMUM_CYCLE_LIMIT = 10000 #a big constant used to limit the iteration loop number
 destination = None  #global variable holds the destination
 flightDBManager = sql.FlightTestingDBManager()
@@ -94,7 +95,7 @@ class node:
         return 0
     a = self.val
     self.currentDatetime = self.flightHistory[self.flightHistory.__len__()-1].arrivalTime
-    if self.currentairport == destination:
+    if self.currentairport.city_name == destination.city_name:
       a += DESTINATION_FLAG
     if not earlyTo(self.currentDatetime, endDatetime):
       a -= TIME_END_FLAG
@@ -107,6 +108,7 @@ def BFS(StartCity,EndCity):
     Should set start datetime and end datetime before calling
   """
   pq = PriorityQueue()#priority queue is a builtin lower first priority queue that handle tuples like (priority, object)
+  global destination
   destination = EndCity
   startNode = node(currentairport=StartCity)
   startNode.currentDatetime = startDatetime
@@ -115,14 +117,19 @@ def BFS(StartCity,EndCity):
   last = 0
   result = []#candidates for result, containing local maximum and global maximum
   while (not pq.empty()) and (count < MAXIMUM_CYCLE_LIMIT):
+    print(count)
     #in each iteration we get the node with best estimation
     #and explore every avaliable flight
     current = pq.get_nowait()[1]
+    if current.currentairport.city_name == destination.city_name  :
+        #pq.put_nowait((-current.eval(),current))
+        if (endDatetime - current.currentDatetime).days < 1:
+            return current
     last = current.eval()
     resultflag = True
     todayAndTomorrow = current.currentairport.getDepartureFlight(current.currentDatetime)
-
-    todayAndTomorrow+=(current.currentairport.getDepartureFlight(current.currentDatetime))
+    tomorrow = util.getTomorrowDatetime(current.currentDatetime)
+    todayAndTomorrow+=(current.currentairport.getDepartureFlight(tomorrow))
     for flight in todayAndTomorrow:
       if earlyTo(current.currentDatetime,flight.departureTime):
         newNode = node(history=current,flight=flight)
@@ -133,6 +140,7 @@ def BFS(StartCity,EndCity):
     if resultflag:#otherwise it's a local maximum or global maximum
       result.append(current)
     count += 1
+
   for a in result:#get all of these maximum value together and return the one with maximum value
     pq.put_nowait((-a.eval(),a))
   return pq.get_nowait()[1]
